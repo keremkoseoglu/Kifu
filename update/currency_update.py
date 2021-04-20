f6bf33497226f2f340d53b7168c51688867b9a66
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2021-04-20 21:47:38 +0300

    currency try catch

--- update/currency_update.py
+++ update/currency_update.py
@@ -13,29 +13,33 @@ def execute():
     if _EXECUTED_TODAY:
         return
 
-    # Currencies from TCMB
-    resp = requests.get(config.CONSTANTS["CURRENCY_CONV_URL"], verify=False)
-    resp_as_dict = xmltodict.parse(resp.text)
+    try:
+        # Currencies from TCMB
+        resp = requests.get(config.CONSTANTS["CURRENCY_CONV_URL"], verify=False)
+        resp_as_dict = xmltodict.parse(resp.text)
 
-    # Gold conversion
-    gold_resp = requests.get(config.CONSTANTS["CURRENY_GOLD_URL"])
-    pos1 = gold_resp.text.find('<table class="table table-striped">') + 113
-    gold_price_txt = gold_resp.text[pos1:pos1+7].replace("<", "").replace(",", ".")
-    gold_price = float(gold_price_txt.replace("/", ""))
-    dgc_dict = {
-        "@CrossOrder": "0",
-        "@Kod": "DGC",
-        "@CurrencyCode": "DGC",
-        "Unit": "1",
-        "Isim": "Gold",
-        "CurrencyName": "Gold",
-        "ForexBuying": gold_price,
-        "ForexSelling": gold_price,
-        "BanknoteBuying": gold_price,
-        "BanknoteSelling": gold_price
-    }
-    resp_as_dict["Tarih_Date"]["Currency"].append(dgc_dict)
+        # Gold conversion
+        gold_resp = requests.get(config.CONSTANTS["CURRENY_GOLD_URL"])
+        pos1 = gold_resp.text.find('<table class="table table-striped">') + 113
+        gold_price_txt = gold_resp.text[pos1:pos1+7].replace("<", "").replace(",", ".")
+        gold_price = float(gold_price_txt.replace("/", ""))
+        dgc_dict = {
+            "@CrossOrder": "0",
+            "@Kod": "DGC",
+            "@CurrencyCode": "DGC",
+            "Unit": "1",
+            "Isim": "Gold",
+            "CurrencyName": "Gold",
+            "ForexBuying": gold_price,
+            "ForexSelling": gold_price,
+            "BanknoteBuying": gold_price,
+            "BanknoteSelling": gold_price
+        }
+        resp_as_dict["Tarih_Date"]["Currency"].append(dgc_dict)
 
-    # Save
-    currency.save_currency_conv(resp_as_dict)
-    _EXECUTED_TODAY = True
+        # Save
+        currency.save_currency_conv(resp_as_dict)
+        _EXECUTED_TODAY = True
+
+    except Exception as error:
+        print("Currency update error: " + error)
