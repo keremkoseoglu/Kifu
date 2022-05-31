c45e3233d8045dcf00eff14f4cc2d8af628839a2
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2022-05-31 17:34:43 +0300

    net worth Fixes #168

--- web/api/net_worth.py
+++ web/api/net_worth.py
@@ -30,6 +30,9 @@ class NetWorthAPI():
         credit_card_debt = credit_card.get_current_credit_card_debt_sum() * -1
         self._append_result("Credit card debt", credit_card_debt)
 
+        # SUM: Liquid cash
+        self._append_sum("Cash")
+
         # Activity earnings
         activity_earning = Activity.get_total_activity_earnings()
         self._append_result("Activity earning", activity_earning)
@@ -43,20 +46,39 @@ class NetWorthAPI():
         payment_balance = payment.get_payment_balance()
         self._append_result("Payment balance", payment_balance)
 
+        # SUM: Short term cash
+        self._append_sum("Money")
+
         # Asset resale
         asset_resales = asset.get_asset_type_resale_value_sum(deduct_income_tax=True,
                                                               own_percentage_only=True)
         for asset_resale in asset_resales:
             self._append_result(asset_resale["type"] + " resale", asset_resale["sales_value"])
 
+        # Reserved balance again
+        reserved_balance *= -1
+        self._append_result("Reserved balance", reserved_balance)
+
+        # SUM: Net worth
+        self._append_sum("Net worth")
+
         # Return
         return self._result
 
-    def _append_result(self, title, value):
+    def _append_result(self, title, value, icon: str = ""):
         self._sum_val += value
 
-        entry = {"title": title,
+        entry = {"icon": icon,
+                 "title": title,
                  "content": value,
+                 "result": self._sum_val}
+
+        self._result["NetWorth"].append(entry)
+
+    def _append_sum(self, title):
+        entry = {"icon": "∑",
+                 "title": title,
+                 "content": 0,
                  "result": self._sum_val}
 
         self._result["NetWorth"].append(entry)
