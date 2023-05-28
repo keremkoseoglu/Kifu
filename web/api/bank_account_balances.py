af9a7bc8e0d043d36e6b0192dfee3827fbe6d175
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2023-05-28 12:48:06 +0300

    fixes #210

--- web/api/bank_account_balances.py
+++ web/api/bank_account_balances.py
@@ -3,19 +3,20 @@ from model import asset
 from model.bank import bank_account
 from web.api.chart import get_pie_dict
 
-class BankAccountBalanceAPI():
-    """ Bank account balance API """
+
+class BankAccountBalanceAPI:
+    """Bank account balance API"""
 
     @property
     def result(self) -> dict:
-        """ Bank account balances """
-        out = {"Balances": [],
-               "PieChart": {}}
+        """Bank account balances"""
+        out = {"Balances": [], "PieChart": {}}
 
         # Balances
         bank_balances = bank_account.get_account_balances_in_both_currencies()
-        asset_balances = asset.get_liquid_assets_in_both_currencies(deduct_income_tax=True,
-                                                                    own_percentage_only=True)
+        asset_balances = asset.get_liquid_assets_in_both_currencies(
+            deduct_income_tax=True, own_percentage_only=True
+        )
 
         for bank_balance in bank_balances:
             out["Balances"].append(bank_balance)
@@ -23,9 +24,9 @@ class BankAccountBalanceAPI():
             out["Balances"].append(asset_balance)
 
         # Pie Chart
-        out["PieChart"] = get_pie_dict(entries=out["Balances"],
-                                       label_fld="name",
-                                       val_fld="home_balance")
+        out["PieChart"] = get_pie_dict(
+            entries=out["Balances"], label_fld="name", val_fld="home_balance"
+        )
 
         # Flush
         return out
