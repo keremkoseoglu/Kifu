3fa01da4fd990719f03448a6f2c47cd82beb5b7b
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2024-03-30 10:23:41 +0300

    general sync

--- util/hosts.py
+++
@@ -1,8 +0,0 @@
-""" Hosts """
-from shutil import copy
-import config
-
-
-def overwrite_host_file():
-    """ Override hosts file """
-    copy(config.CONSTANTS["HOSTS_FROM"], config.CONSTANTS["HOSTS_TO"])
